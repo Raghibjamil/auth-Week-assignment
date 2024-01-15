312e281098d86178bc44b133fcbdb6d18278f4ef
Raghibjamil <raghibj123@gmail.com>
2024-01-15 22:07:20 +0530

    added

---
+++ Server/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+require("dotenv").config()
+const connectDatabase = require("./config/db");
+const { userRoute } = require("./routes/user.route");
+const cors = require("cors")
+const app = express();
+const cookieParser = require("cookie-parser")
+
+app.use(cors({
+    origin:["http://localhost:5500","http://127.0.0.1:5500"],
+    // origin:"http://127.0.0.1:5500",
+
+    credentials:true
+}))
+app.use(cookieParser())
+
+app.use(express.json())
+app.use("/",userRoute)
+
+
+
+app.listen(process.env.PORT,async()=>{
+  connectDatabase()
+    console.log("Server Starting on PORT :",process.env.PORT)
+})
